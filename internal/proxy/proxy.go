202215cef4a70a8447c71b3dd69df486f92fe61a
Ivan Daniel Zenger <ivan.zenger.1@postfinance.ch>
2024-02-28 14:22:56 +0100

    chore: debug log

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -44,6 +44,8 @@ func Setup(e *echo.Echo, cfg *config.Config, l *zap.SugaredLogger) {
 			return c.Redirect(http.StatusFound, cfg.AuthEndpoint)
 		}
 
+		l.Debug(token)
+
 		claims, err := jwks.ParseJWTToken(token, cfg.JwksUrl)
 		if err != nil {
 			l.Error("Error parsing token:", err)
